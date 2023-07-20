a58e67e64a4d451757c59ff0feeb21ba69f49cb5
riyaz7788 <syed.riyaz@makear.io>
2023-07-20 14:04:31 +0530

    hershey landing page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const app = express()
 const router = require('./router')
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
