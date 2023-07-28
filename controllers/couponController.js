b96e5cd9de7d52e9e0f2482878d051aa8964f94d
Srishti Jain <srishti@impresariotech.io>
2023-07-28 19:42:40 +0530

    coupon loading in one page

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const couponModel = require('../models/Coupon');
-let couponMessage = '';
+//let couponMessage = '';
 //public routes
+// router.get('/', (req, res) => {
+//     couponMessage = ""
+//     res.render('get-coupon', { couponMessage });
+//   })
+// router.post('/get-coupon', (req, res) => {
+//   const visitorCoupon = req.cookies.coupon;
+//   const { visitorCoupon: newVisitorCoupon, couponMessage } = couponModel.getCoupon(visitorCoupon);
+//   res.cookie('coupon', newVisitorCoupon, { maxAge: 86400000 });
+//   res.render('get-coupon', { couponMessage });
+//   res.json({ couponMessage });
+// });
 router.get('/', (req, res) => {
-    couponMessage = ""
-    res.render('get-coupon', { couponMessage });
-  })
-router.post('/get-coupon', (req, res) => {
-  const visitorCoupon = req.cookies.coupon;
-  const { visitorCoupon: newVisitorCoupon, couponMessage } = couponModel.getCoupon(visitorCoupon);
-  res.cookie('coupon', newVisitorCoupon, { maxAge: 86400000 });
-  res.render('get-coupon', { couponMessage });
-});
+    res.render('get-coupon');
+  });
+  
+  router.get('/get-coupon', (req, res) => {
+    const visitorCoupon = req.cookies.coupon;
+    const { visitorCoupon: newVisitorCoupon, couponMessage } = couponModel.getCoupon(visitorCoupon);
+    res.cookie('coupon', newVisitorCoupon, { maxAge: 86400000 });
+    res.json({ couponMessage });
+  });
+  
 
 module.exports = router;
