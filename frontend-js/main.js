4838884e7d1265eb334280ed02d819cf7917cdca
kartikb123 <121477682+kartikb123@users.noreply.github.com>
2023-07-31 13:31:13 +0530

    type of model functionality

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -606,18 +606,22 @@ nextbtn.onclick = () => {
 
 if(typeofpack==='kisses'){
 //
+modelname.setAttribute('gltf-model','/models/kisses .glb')
+console.log("ss")
 thirdscreen.style.display = "block"
 headerlogo.src='/images/hersheys-kisses-logo 1.png'
 headerlogo.classList.add("headerlogokisses");
 
 }else if(typeofpack==='chocolatebar')
 {
+  modelname.setAttribute('gltf-model','/models/kisses .glb')
 thirdscreenbar.style.display = "block"
 headerlogo.src='/images/hersheyslogo.png'
 headerlogo.classList.add("headerlogo-bar-exotic");
 }else if(typeofpack==='exotic')
 {
   //
+  modelname.setAttribute('gltf-model','/models/kisses .glb')
   thirdscreenbar.style.display = "block"
   headerlogo.src='/images/hed-logo.png'
   headerlogo.classList.add("headerlogo-bar-exotic");
