b5dea08786f4cd038b2368dd2311218e5b19f953
Srishti Jain <srishti@impresariotech.io>
2023-08-02 14:20:11 +0530

    coupon added

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,10 +1,405 @@
 const couponsArray = [
-    { "coupon": "COUPON1", "isRedeem": false },
-    { "coupon": "COUPON2", "isRedeem": false },
-    { "coupon": "COUPON3", "isRedeem": false },
-    { "coupon": "COUPON4", "isRedeem": false },
-    { "coupon": "COUPON5", "isRedeem": false }
-  ];
+  {
+   "coupon": "8CWBKWGWA2",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8YGZPA246P",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8UZLWTGTYR",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8PPNHMKCLE",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EF88LTKHF",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EC93297SP",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EMKRA2XK3",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8DQNV3FX6J",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8KRMWUQN23",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8WFVBEVFJT",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EKJ9RULH2",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8LJXSK7YDG",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8KDG68JNP8",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8RD682QYUZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8NRZEKLE3Y",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8DKHARNHXE",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8MPDASSHYZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ZXN64ZBLH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ER2T2HMTB",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8JBG6GB7ZB",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QDRH4K33K",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8CP4DA7GD6",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8AUMKYMVTE",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8GAGLJ3Z7S",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8XL69K9E5F",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EQXBUN7GZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8VQT64NN5B",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8HXFHDDGQ3",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8PTBDHJ9WL",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BN7XTYQFD",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8TYX8EVF2P",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8PSTGZAKLZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8YYWHQRJKB",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8JZW8SPJB3",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8XYYT7FEFP",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QBFKMD2QY",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8JRQCL2PMB",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8CUZMRVKZ5",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8UE8YN2RRY",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8NGQRPTNCH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BNPAQUWDN",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8VPGECTESC",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BMB4P4S3Q",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ZH6D6HVZ9",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8MADHGVMX3",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8PFWM8SALG",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8MLEZ6ZH6Z",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BWSKWKRVG",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ZSR9AGHDH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8USFXULWXR",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8LMA7H7FJN",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BDGWFLUQ9",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8MKNXJVTBA",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8MU8UX6P4T",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8JM7X3PZHH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ECBJV5UJH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ZBRHH5LW5",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8AFGKD65NN",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8YTSCV6BZM",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8GRSYJXFBC",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8GPDBAL8AJ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BX363BFBN",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8NTCSDKAZN",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QU2CHAPPZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8KF2WRRPBR",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ZKYPEDERA",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8FDA7QNMA9",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8WM9FXM9LL",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8LE8MUDU6H",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BJGCHQ8BF",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8VWAAZFQV6",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8RVM4S4RJH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8JV3V3LWA5",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8PML5C2NMZ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8KC2YV7BYE",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8TNL79LHJM",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QK8UCL7D2",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8TK6L2N8UF",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8CT55FWVAR",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EBD3BJH2S",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8NJTGGGUQA",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8VZ8Y3CXXG",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QVLBG228G",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8QJKFFV8H8",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8GEXACRZTC",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8DKB8WR9VE",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8VYZK584AS",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8FKT99PPZU",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8YA9KDYU7A",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8CA5QAPRL8",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8ET46H9N8W",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8UCVPTAR3W",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8TMNR2R7HQ",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8RYMXPADCK",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8FXSUGX82W",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8NLUEPCAZX",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8BDM8TTW69",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8XSEH3WZCX",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8EN7ZZPAAH",
+   "isRedeem": false
+  },
+  {
+   "coupon": "8RJQMSBUAN",
+   "isRedeem": false
+  }
+ ];
   
   function getCoupon(visitorCoupon) {
     let couponMessage = '';
@@ -15,6 +410,7 @@ const couponsArray = [
           visitorCoupon = couponsArray[i].coupon;
           couponsArray[i].isRedeem = true;
           couponMessage = ` ${visitorCoupon}`;
+          console.log(couponsArray)
           break;
         }
       }
