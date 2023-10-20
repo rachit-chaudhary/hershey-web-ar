89d7cc7405c8eb432886b6409b4bc784e4234215
Rachit <mailracit@gmail.com>
2023-10-20 13:19:13 +0530

    password protection added

--- router.js
+++ router.js
@@ -2,6 +2,16 @@ const express =require('express')
 const emailController = require('./controllers/emailController')
 const router = express.Router()
 
+function passwordProtected(req, res, next) {
+    res.set("WWW-Authenticate", "Basic realm ='makearsiteejs")
+    if (req.headers.authorization == "Basic YWRtaW46cXdlcnR5cXdlcnR5ODk=") {
+        next()
+    } else {
+        console.log(req.headers.authorization)
+        res.status(401).send("Try again")
+    }
+}
+
 //home route
 router.get('/', (req, res) => {
     res.render('index')
@@ -15,7 +25,7 @@ router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-router.get('/view-email-address', (req, res) => {
+router.get('/view-email-address', passwordProtected, (req, res) => {
     res.render('email-list')
 })
 
