a695925ec1280557493446bb610ca49792d1d473
prashant <prashant.sharma@makear.ai>
2024-09-09 14:43:06 +0530

    reduced animtime by 1 more

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -368,7 +368,8 @@ hoverChange.onclick = () => {
   hoverExotic.src = '/images/exotic.png'
 
   typeofpack = 'kisses'
-  animtime = 12000
+  // animtime = 12000
+  animtime = 11000
   pNametype = "1"
 }
 // -----------------------------pack2 hershey--------------
@@ -397,7 +398,8 @@ hoverexotic.onclick = () => {
   // hoverHershey.src = '/images/hershey.png'
 
   typeofpack = 'exotic'
-  animtime = 16000
+  // animtime = 16000
+  animtime = 15000
   pNametype = "3"
 
 }
@@ -1523,7 +1525,8 @@ AFRAME.registerComponent("swap-texture", {
       if (pNametype === "1") {
         console.log("pnametype" + pNametype);
         dataURL = pName
-        animtime = 12000
+        // animtime = 12000
+        animtime = 11000
         modelname.setAttribute('gltf-model', '/models/kissesfile3flip.glb')
         hereGoesID.innerHTML = `${name}`
         //option1
@@ -1560,7 +1563,8 @@ AFRAME.registerComponent("swap-texture", {
       else if (pNametype === "2") {
         console.log("pnametype" + pNametype);
         dataURL = pName
-        animtime = 13000
+        // animtime = 13000
+        animtime = 12000
         modelname.setAttribute('gltf-model', '/models/barsfinalfile5new.glb')
         hereGoesID.innerHTML = `${name}`
         //option1
@@ -1602,7 +1606,8 @@ AFRAME.registerComponent("swap-texture", {
       else if (pNametype === "3") {
         console.log("pnametype" + pNametype);
         dataURL = pName
-        animtime = 16000
+        // animtime = 16000
+        animtime = 15000
         modelname.setAttribute('gltf-model', '/models/edfinal3new.glb')
         hereGoesID.innerHTML = `${name}`
         //option1
