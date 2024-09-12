50bc599abb48ebb84cf4184de55389ef66e01a39
prashant <prashant.sharma@makear.ai>
2024-09-12 10:18:36 +0530

    removed isMobileOnly sript

--- frontend-js/oldinitRecorder.js
+++ frontend-js/oldinitRecorder.js
@@ -1,109 +1,109 @@
-async function initRecorder() {
+// async function initRecorder() {
 
-  const canvas = document.querySelector('canvas') || document.createElement('canvas');
-//   // const url = canvas.toDataURL('video/mp4', 0.8);
-//   var width = window.innerWidth
-//   || document.documentElement.clientWidth
-//   || document.body.clientWidth;
-// var height = window.innerHeight
-//   || document.documentElement.clientHeight
-//   || document.body.clientHeight;
-// // Get canvas from dom
-// document.querySelector("a-scene").setAttribute('screenshot', {
-//   width: width,
-//   height: height
-// })
-// const canvas = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
+//   const canvas = document.querySelector('canvas') || document.createElement('canvas');
+// //   // const url = canvas.toDataURL('video/mp4', 0.8);
+// //   var width = window.innerWidth
+// //   || document.documentElement.clientWidth
+// //   || document.body.clientWidth;
+// // var height = window.innerHeight
+// //   || document.documentElement.clientHeight
+// //   || document.body.clientHeight;
+// // // Get canvas from dom
+// // document.querySelector("a-scene").setAttribute('screenshot', {
+// //   width: width,
+// //   height: height
+// // })
+// // const canvas = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
 
-  let recording = false;
+//   let recording = false;
 
-  const recorder = await ZapparVideoRecorder.createCanvasVideoRecorder(canvas, {
-    quality:10,
-    maxFrameRate:144,
-    audio: false,
-  })
+//   const recorder = await ZapparVideoRecorder.createCanvasVideoRecorder(canvas, {
+//     quality:10,
+//     maxFrameRate:144,
+//     audio: false,
+//   })
 
-  // When we start recording update text
+//   // When we start recording update text
 
-  recorder.onStart.bind(() => {
+//   recorder.onStart.bind(() => {
 
-    recording = true;
+//     recording = true;
 
-    console.log("start 2")
+//     console.log("start 2")
 
-    // placeButton.innerText = 'TAP TO STOP RECORDING';
+//     // placeButton.innerText = 'TAP TO STOP RECORDING';
 
-  });
-
- 
-
-  // When stop recording update text, and prompt a social share dialog.
-
-  recorder.onComplete.bind(async (result) => {
+//   });
 
  
 
-    // placeButton.innerText = 'TAP TO START RECORDING';
+//   // When stop recording update text, and prompt a social share dialog.
 
-    console.log("stop 2")
-
- 
-
-    // result.arrayBuffer
-
-    console.log(result.asDataURL())
+//   recorder.onComplete.bind(async (result) => {
 
  
 
-    ZapparWebGLSnapshot({
+//     // placeButton.innerText = 'TAP TO START RECORDING';
 
-      data: await result.asDataURL(),
-
-      fileNamePrepend: 'hersheys_sibling_surprise',
-
-      // data:url,
-
-      onClose: () => {
-
-        console.log('Dialog was closed');
-
-      },
-
-    });
+//     console.log("stop 2")
 
  
 
-    // console.log(recorder._getData())
+//     // result.arrayBuffer
 
-    recording = false;
-
-  });
+//     console.log(result.asDataURL())
 
  
 
-  // Toggle between recording
+//     ZapparWebGLSnapshot({
 
-  capture.addEventListener('click', async () => {
-    if (recording) {
+//       data: await result.asDataURL(),
 
-      recorder.stop();
+//       fileNamePrepend: 'hersheys_sibling_surprise',
 
-      console.log("stop")
+//       // data:url,
 
-      // capture.style.display = 'none'
+//       onClose: () => {
 
-      capture.src = "/images/shutter-button-start.png"
+//         console.log('Dialog was closed');
 
-    } else {
+//       },
 
-      recorder.start();
+//     });
 
-      capture.src = "/images/shutter-button-stop.png"
+ 
 
-      console.log("start")
+//     // console.log(recorder._getData())
 
-    }
+//     recording = false;
 
-  });
+//   });
 
-}
+ 
+
+//   // Toggle between recording
+
+//   capture.addEventListener('click', async () => {
+//     if (recording) {
+
+//       recorder.stop();
+
+//       console.log("stop")
+
+//       // capture.style.display = 'none'
+
+//       capture.src = "/images/shutter-button-start.png"
+
+//     } else {
+
+//       recorder.start();
+
+//       capture.src = "/images/shutter-button-stop.png"
+
+//       console.log("start")
+
+//     }
+
+//   });
+
+// }
